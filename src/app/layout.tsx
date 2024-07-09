3a4b686c5f0ca24449ce0b8e6bc8b1893b3d526e
Matheus Alves <matheusfrancisco0330@gmail.com>
2024-07-09 10:52:31 -0300

    feat: config SEO

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,8 +6,22 @@ import { Header } from "@/components/header/Header";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "devGames",
+  title: "devGames - Descubra jogos incríveis para se divertir",
   description: "O lugar para conhecer novos games para o seu dia!",
+  keywords: ['games', 'jogos', 'steam'],
+  openGraph: {
+    images: [`${process.env.PUBLIC_URL}/preview.png`]
+  },
+  robots: {
+    googleBot: {
+      index: true,
+      follow: true,
+      noimageindex: true,
+    },
+    nocache: true,
+    follow: true,
+    index: true
+  }
 };
 
 export default function RootLayout({
