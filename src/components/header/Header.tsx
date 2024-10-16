d98c0ce2ee342857f0ee3c4ea670eb06f993c913
Matheus Alves <matheusfrancisco0330@gmail.com>
2024-10-16 13:19:03 -0300

    fix: remove unecessary routes

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,7 +1,7 @@
 import Link from "next/link";
 import Image from "next/image";
 import LogoImg from '@/assets/logo.svg';
-import { LiaGamepadSolid} from 'react-icons/lia';
+import { LiaGamepadSolid } from 'react-icons/lia';
 export function Header() {
     return (
         <header className="w-full h-28 bg-slate-100 text-black px-2">
@@ -16,19 +16,19 @@ export function Header() {
                             className="w-full"
                         />
                     </Link>
-                    <Link href='/games'>
+                    {/* <Link href='/games'>
                     Games
                     </Link>
                     <Link href='/profile'>
                     Perfil
-                    </Link>
+                    </Link> */}
                 </nav>
                 <div className="hidden sm:flex justify-center items-center">
-                    <Link href='/profile'>
-                    <LiaGamepadSolid
-                        size={32}
-                        color="#475569"
-                    />
+                    <Link href='/'>
+                        <LiaGamepadSolid
+                            size={32}
+                            color="#475569"
+                        />
                     </Link>
                 </div>
             </div>
